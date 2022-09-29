fc79fe07eb4f008f725f66a2d221fa2242287772
A. Bullock <mondeendeguise@gmail.com>
2022-09-29 14:20:55 -0600

    Update setup_arch.sh

--- setup_arch.sh
+++ setup_arch.sh
@@ -2,10 +2,13 @@
 
 # Install packages
 pacman -Syu
-pacman -S git tree zsh neovim tmux
+pacman -S git tree zsh neovim tmux wget base-devel docker
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Enable sudo privileges for wheel group
-set 's/# %wheel ALL=(ALL:ALL) ALL /%wheel ALL=(ALL:ALL) ALL' /etc/sudoers
+sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL' /etc/sudoers
 
 # Add non-root user
 read username
@@ -14,8 +17,11 @@ passwd $username
 
 # Make symlinks to dotfiles repo
 git clone https://github.com/mondeendeguise/dotfiles /home/$username
-mkdir -p /home/$username/.config/
+mkdir /home/$username/.config/
 ln -s /home/$username/dotfiles/nvim /home/$username/.config/nvim
 ln -s /home/$username/dofiles/.zshrc /home/$username/.zshrc
 ln -s /home/$username/dotfiles/.gitconfig /home/$username/.gitconfig
 
+# Install packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+/home/$username/.local/share/nvim/site/pack/packer/start/packer.nvim
