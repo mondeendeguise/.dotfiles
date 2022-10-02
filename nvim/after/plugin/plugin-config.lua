02e80d8ff6566cf637f1220fb250bf52fb0f3476
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2022-10-02 16:03:50 -0600

    move plugin configs to after dir

--- nvim/after/plugin/plugin-config.lua
+++ nvim/after/plugin/plugin-config.lua
@@ -2,7 +2,52 @@ local g = vim.g
 
 require('lualine').setup()
 
--- g.ranger_map_keys = 0
--- g.ranger_replace_netrw = 1
-
 vim.cmd('colorscheme gruvbox')
+
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to 'ensure_installed')
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = {},
+
+  highlight = {
+    -- 'false' will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    disable = {},
+
+    additional_vim_regex_highlighting = true,
+  },
+  indent = {
+    enable = true,
+  }
+}
+-- Folds
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- LSP
+require('nvim-lsp-installer').on_server_ready(function(server)
+  local opts = {}
+  if server.name == 'sumneko_lua' then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+        }
+      }
+    }
+  end
+  server:setup(opts)
+end)
