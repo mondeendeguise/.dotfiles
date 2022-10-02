1ac2c1cedc7a252b2e70d50c80e7ecb977c3dcd2
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2022-10-02 15:44:00 -0600

    start using treesitter and fix lsp

--- nvim/lua/mondeendeguise/init.lua
+++ nvim/lua/mondeendeguise/init.lua
@@ -2,11 +2,62 @@ require('mondeendeguise.plugins')
 require('mondeendeguise.settings')
 require('mondeendeguise.keybinds')
 
--- lsp setup
-require'lspconfig'.pyright.setup {}
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    -- settings go here
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust" },
+
+  -- Install parsers synchronously (only applied to 'ensure_installed')
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = {},
+
+  highlight = {
+    -- 'false' will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    disable = {},
+
+    additional_vim_regex_highlighting = true,
+  },
+  indent = {
+    enable = true,
   }
 }
-require'lspconfig'.rust_analyzer.setup {}
+
+-- treesitter folds
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- lsp setup
+require('nvim-lsp-installer').on_server_ready(function(server)
+  local opts = {}
+  if server.name == 'sumneko_lua' then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+        }
+      }
+    }
+  end
+  server:setup(opts)
+end)
+
+-- require'lspconfig'.sumneko_lua.setup {
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = { 'vim' }
+--       }
+--     }
+--   }
+-- }
+-- require'lspconfig'.pyright.setup {}
+-- require'lspconfig'.rust_analyzer.setup {}
--- nvim/lua/mondeendeguise/plugins.lua
+++ nvim/lua/mondeendeguise/plugins.lua
@@ -3,12 +3,23 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
     -- lsp
-    use 'neovim/nvim-lsp'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
 
 	-- bless the lord tpope
 	use 'tpope/vim-surround'
 	use 'tpope/vim-commentary'
 	use 'tpope/vim-repeat'
+
+    -- file explorer
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons',
+      },
+      tag = 'nightly'
+    }
 
 	-- groovy status line
 	use {
