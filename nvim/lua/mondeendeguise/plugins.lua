2eccc619b068bd56172b5382409af15d332296d5
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2022-10-06 17:01:45 -0600

    fix formatting, add rust plugin

--- nvim/lua/mondeendeguise/plugins.lua
+++ nvim/lua/mondeendeguise/plugins.lua
@@ -1,32 +1,35 @@
 return require('packer').startup(function(use)
-	-- packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    -- lsp
-    use 'nvim-treesitter/nvim-treesitter'
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
+  -- langs
+  use 'rust-lang/rust.vim'
 
-	-- bless the lord tpope
-	use 'tpope/vim-surround'
-	use 'tpope/vim-commentary'
-	use 'tpope/vim-repeat'
+  -- lsp
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
 
-    -- file explorer
-    use {
-      'kyazdani42/nvim-tree.lua',
-      requires = {
-        'kyazdani42/nvim-web-devicons',
-      },
-      tag = 'nightly'
-    }
+  -- bless the lord tpope
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-repeat'
 
-	-- groovy status line
-	use {
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
+  -- file explorer
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+    },
+    tag = 'nightly'
+  }
 
-	-- colorscheme
-	use 'gruvbox-community/gruvbox'
+  -- groovy status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- colorscheme
+  use 'gruvbox-community/gruvbox'
 end)
