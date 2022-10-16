681d1633eb1f1702e685eb8e46894866b245ac19
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2022-10-15 19:31:55 -0600

    adding folds, goodbye <c-j,k>, insert trailing character from normal mode

--- nvim/lua/mondeendeguise/keybinds.lua
+++ nvim/lua/mondeendeguise/keybinds.lua
@@ -4,15 +4,11 @@ local function nmap(key, kmap)
   map('n', key, kmap, opts)
 end
 
--- Newlines
-map('n', '<c-j>', 'o<esc>', opts)
-map('n', '<c-k>', 'O<esc>', opts)
-
 -- Change indent continuously
 map('v', '<', '<gv', {})
 map('v', '>', '>gv', {})
 
--- LSP keybinds
+-- LSP keybinds {{{
 nmap('gd', ':lua vim.lsp.buf.definition()<cr>')
 nmap('gD', ':lua vim.lsp.buf.declaration()<cr>')
 nmap('gi', ':lua vim.lsp.buf.implementation()<cr>')
@@ -21,15 +17,16 @@ nmap('gw', ':lua vim.lsp.buf.workspace_symbol()<cr>')
 nmap('gr', ':lua vim.lsp.buf.references()<cr>')
 nmap('gt', ':lua vim.lsp.buf.type_definition()<cr>')
 nmap('K', ':lua vim.lsp.buf.hover()<cr>')
--- nmap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
+nmap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
 nmap('<leader>af', ':lua vim.lsp.buf.code_action()<cr>')
 nmap('<leader>rn', ':lua vim.lsp.buf.rename()<cr>')
 
 nmap('<leader>ff', ':lua vim.lsp.buf.format()<cr>')
+-- }}}
 
--- Insert trailing ; or , from insert mode
-map('i', ';;', '<esc>A;<esc>', {})
-map('i', ',,', '<esc>A,<esc>', {})
+-- Insert trailing ; or , from normal mode
+map('n', '<leader>;', '<esc>A;<esc>', {})
+map('n', '<leader>,', '<esc>A,<esc>', {})
 
 -- TagBar
 map('n', '<leader>tb', ':TagbarToggle()<cr>', opts)
