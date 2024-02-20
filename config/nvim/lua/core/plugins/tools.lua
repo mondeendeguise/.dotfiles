987af59e62f01b117d838f5548d9977022ac3cae
Ash Bullock <45837222+mondeendeguise@users.noreply.github.com>
2024-02-20 11:21:30 -0700

    tool

---
+++ config/nvim/lua/core/plugins/tools.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "timetro/glslView-nvim",
+        ft = {
+            "glsl",
+            "vert",
+            "frag",
+            "ply",
+            "lst",
+            "obj",
+            "gltf",
+            "glb",
+            "shader",
+        },
+    },
+}
