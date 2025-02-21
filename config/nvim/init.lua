5b18cf6b8aaedbc4e2426b5d7878cb1b3150482f
Ash Bullock <mondeendeguise@gmail.com>
2025-02-21 12:39:38 -0700

    simplify neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,60 @@
-require("core")
+vim.o.autoindent = true
+vim.o.smarttab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.laststatus = 2
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.numberwidth = 4
+
+vim.o.cursorcolumn = true
+vim.o.cursorline = true
+vim.o.guicursor = ''
+
+vim.o.wrap = false
+vim.o.scrolloff = 8
+
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+
+vim.g.mapleader = ' '
+
+vim.g.netrw_banner = 0
+vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Explore, {})
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable',
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    { 'tpope/vim-fugitive', cmd = 'Git', },
+    'tpope/vim-endwise',
+    'tpope/vim-abolish',
+    'tpope/vim-speeddating',
+    'tpope/vim-surround',
+    'tpope/vim-repeat',
+
+    'christoomey/vim-tmux-navigator',
+
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, opts = { transparent_background = true, }, init = function() vim.cmd([[colorscheme catppuccin]]) end, },
+})
--- config/nvim/lua/core/keymap.lua
+++
@@ -1,8 +0,0 @@
-local k = vim.keymap
-
-local opts = {
-    noremap = true,
-    silent = true,
-}
-
-k.set("n", "<Leader>pv", vim.cmd.Ex, opts)
